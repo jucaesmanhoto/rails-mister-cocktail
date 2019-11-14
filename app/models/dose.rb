128e8fb3a67093bbaf83bcff79d1e36778895646
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-14 13:06:04 -0300

    models and cocktails controller up n running

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true, allow_blank: false
+  validates :ingredient, presence: true
+  validates :cocktail, presence: true
+  validates_uniqueness_of :ingredient, scope: :cocktail
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+response = open(url).read
+data = JSON.parse(response)['drinks']
+
+data.each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
+
+puts "created #{Ingredient.count} ingredients"
